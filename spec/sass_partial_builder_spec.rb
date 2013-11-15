5017c5199e29ccf6b0244f2d06d013cd62b1fb3d
Adam Hendricksen <adam@nird.us>
2013-11-15 15:40:31 -0800

    Linking to file in sibling folder

---
+++ spec/sass_partial_builder_spec.rb
@@ -0,0 +1,8 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_partial_builder')
+
+describe "BundlerSass" do
+  it "fails" do
+    fail "hey buddy, you should probably rename this file and start specing for real"
+  end
+end
