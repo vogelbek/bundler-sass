9d43a07b207d71a694aa249e071c7940c8ca71eb
Adam Hendricksen <adam@nird.us>
2013-11-15 15:51:33 -0800

    First test

--- spec/sass_partial_spec.rb
+++ spec/sass_partial_spec.rb
@@ -3,7 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_partial')
 
 describe "SassPartial" do
   include SassPartial
+  before(:each) do
+    partial_1 = SassPartial.new
+  end
   it "should be a type of file" do
-    SassPartial
+    partial_1.should be_a(File)
   end
 end
