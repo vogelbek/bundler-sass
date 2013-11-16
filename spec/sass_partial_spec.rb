953de51222d04ac970581269db5b91a4e1ab03c2
Adam Hendricksen <adam@nird.us>
2013-11-15 16:27:18 -0800

    Building up test files, and removing them after. Passing creation test

--- spec/sass_partial_spec.rb
+++ spec/sass_partial_spec.rb
@@ -4,10 +4,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_partial')
 describe "SassPartial" do
   context "create and destroy" do
     before(:each) do
-      partial_1 = SassPartial.new "/test_files/_1.sass", "w"
+      Dir.mkdir("test_files")
+      Dir.chdir("test_files")
+      partial_1 = SassPartial.new "_1.sass", "w"
+      Dir.chdir("..")
     end
     it "should create a file" do
-      Dir.entries("test_files").should eq ["..", ".", "_1.sass"]
+      Dir.entries("test_files").include?("_1.sass").should eq true
+    end
+    after(:all) do
+      Dir.foreach("test_files")do |partial|
+        File.delete("test_files/" + partial) unless partial == '.' or partial == '..'
+      end
+      Dir.rmdir("test_files/")
     end
   end
 end
