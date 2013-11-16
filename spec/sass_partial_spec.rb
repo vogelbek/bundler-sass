f6d894c8eb04a8a710f8bb1100ee3448f15dcfbe
Adam Hendricksen <adam@nird.us>
2013-11-15 17:05:21 -0800

    Think the file structure problems are fixed

--- spec/sass_partial_spec.rb
+++ spec/sass_partial_spec.rb
@@ -3,20 +3,22 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_partial')
 
 describe "SassPartial" do
   context "create and destroy" do
-    before(:each) do
+    before(:all) do
+      @root_dir = Dir.getwd
       Dir.mkdir("test_files")
     end
     it "should create multiple files" do
       Dir.chdir("test_files")
       SassPartial.new ["_1.sass", "_2.sass"]
-      Dir.chdir("..")
+      Dir.chdir(@root_dir)
       Dir.entries("test_files").include?("_2.sass").should eq true
     end
     after(:all) do
-      Dir.foreach("test_files")do |partial|
+      Dir.chdir(@root_dir) # in case test has failed and couldn't chdir back to root
+      Dir.foreach("test_files") do |partial|
         File.delete("test_files/" + partial) unless partial == '.' or partial == '..'
       end
-      Dir.rmdir("test_files/")
+      Dir.rmdir("test_files")
     end
   end
 end
