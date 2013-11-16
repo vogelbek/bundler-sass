0cb64bc9c2d2a20756e9c687197411ee81a6bcf0
Adam Hendricksen <adam@nird.us>
2013-11-15 16:37:35 -0800

    Making failing test for batch file create

--- spec/sass_partial_spec.rb
+++ spec/sass_partial_spec.rb
@@ -5,12 +5,12 @@ describe "SassPartial" do
   context "create and destroy" do
     before(:each) do
       Dir.mkdir("test_files")
-      Dir.chdir("test_files")
-      partial_1 = SassPartial.new "_1.sass", "w"
-      Dir.chdir("..")
     end
-    it "should create a file" do
-      Dir.entries("test_files").include?("_1.sass").should eq true
+    it "should create multiple files" do
+      Dir.chdir("test_files")
+      SassPartial.new ["_1.sass", "_2.sass"]
+      Dir.chdir("..")
+      Dir.entries("test_files").include?("_2.sass").should eq true
     end
     after(:all) do
       Dir.foreach("test_files")do |partial|
