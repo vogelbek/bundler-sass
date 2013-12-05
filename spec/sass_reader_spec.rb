8306a753b38b214df95e84f27182dbae04858136
Adam Hendricksen <adam@nird.us>
2013-12-05 15:48:58 -0800

    Setting up testing for sass reader system

---
+++ spec/sass_reader_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_creator')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_reader')
+
+describe "SassReader" do
+  
+end
