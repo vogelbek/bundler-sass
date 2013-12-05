ea6ee79a5b47d7c00db3f5db8adbae0734c8dca1
Adam Hendricksen <adam@nird.us>
2013-12-04 21:49:56 -0800

    Best to include the code with the test...

--- lib/sass_partial.rb
+++ lib/sass_partial.rb
@@ -1,9 +1,19 @@
 class SassPartial
   
-  def self.new file_hash
+  def initialize file_hash
     file_hash.each do |file, dependancy_list| 
-      File.open(file, 'w')
+      File.open file, 'w'
     end
     @dependancies = file_hash
+  end
+
+  def build_imports
+    @dependancies.each do |file, dependancy_list|
+      dependancy_list.each do |dependancy|
+        File.open file, 'r+' do |line|
+          line.write "//import \"#{dependancy.to_s}\""
+        end
+      end
+    end
   end
 end
