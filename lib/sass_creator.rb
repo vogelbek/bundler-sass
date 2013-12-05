d5ca7311a27fb2179301b8cdad497a22fd42336a
Adam Hendricksen <adam@nird.us>
2013-12-05 12:54:18 -0800

    Passing @import test

--- lib/sass_creator.rb
+++ lib/sass_creator.rb
@@ -18,4 +18,16 @@ class SassCreator
       
     end
   end
+
+  def build_directives
+    @dependancies.each do |file, dependancy_list|
+
+      File.open(file, 'r+') do |file|
+        dependancy_list.each do |dependancy|
+          file.write "@import \"#{dependancy.to_s}\"\n"
+        end
+      end
+
+    end
+  end
 end
