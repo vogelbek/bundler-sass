e8149694de830504540e044b640295cb4a127d4d
Adam Hendricksen <adam@nird.us>
2013-12-06 13:42:08 -0800

    Starting work on the tree processing system

---
+++ spec/sass_tree_spec.rb
@@ -0,0 +1,23 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_tree')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_creator')
+
+describe "SassTree" do
+  before(:all) do
+    build_test_directory
+  end
+  before(:each) do
+    enter_test_directory
+  end
+
+  context "extract dependency hashes from files" do
+
+  end
+
+  after(:each) do
+    leave_test_directory
+  end
+  after(:all) do
+    teardown_test_directory
+  end
+end
