1fdd1e9629175a155084f4e0bce880649785388c
Adam Hendricksen <adam@nird.us>
2013-12-06 13:44:11 -0800

    I KNEW that looked like an inject :D

--- lib/sass_reader.rb
+++ lib/sass_reader.rb
@@ -1,7 +1,6 @@
 module SassReader
   def self.dependencies file
-    array = []
-    File.open(file, 'r').each do |line|
+    array = File.open(file, 'r').inject(Array.new) do |array, line|
       array << line if line =~ /\/\/import \"/
     end
     clean_array = array.map {|entry| entry.gsub(/\/\/import\s\"/,"").gsub(/\"\n/,"")}
