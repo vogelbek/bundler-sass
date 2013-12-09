2d9003132111fc45a8566a77a53991abb3da2cf4
Adam Hendricksen <adam@nird.us>
2013-12-09 13:49:40 -0800

    not that hard to make more regex

--- lib/sass_reader.rb
+++ lib/sass_reader.rb
@@ -19,7 +19,7 @@ module SassReader
           array << "#{file}/#{nested_file}"
         end
       else
-        array << file if file =~ /\A_/ and file =~ /.sass\z/ 
+        array << file if file =~ /\A_/ and (file =~ /.sass\z/ or file =~ /.scss\z/) 
         #http://rubular.com/r/DMlfsegYIK for underscore at start
         #http://rubular.com/r/fRM0JX3Nv3 for .sass at end (catches common .css.sass)
       end
