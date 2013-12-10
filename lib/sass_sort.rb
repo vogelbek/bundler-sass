2fd20f42c482c2a7a310f3e82a97ac3d1fdf84be
Adam Hendricksen <adam@nird.us>
2013-12-10 10:19:48 -0800

    Testing first draft of recursive depth first search

--- lib/sass_sort.rb
+++ lib/sass_sort.rb
@@ -3,6 +3,21 @@ require File.expand_path(File.dirname(__FILE__) + '/sass_reader')
 class SassSort
   include SassReader
   def self.import_order
-    SassReader.list_partials
+    @unsorted = SassReader.list_partials
+    @dependencies = SassReader.build_dependency_hash @unsorted
+    depth_first @dependencies
+  end
+
+  private
+  def self.depth_first dependency_array
+    dependency_array.inject([]) do |sorted, file|
+      local_dependencies = SassReader.dependencies file
+      if local_dependencies
+        depth_first local_dependencies
+      else
+        sorted << file
+      end
+      sorted
+    end
   end
 end
