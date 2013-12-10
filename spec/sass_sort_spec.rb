f045a425cc21a48c5a4b03283964c426078882ea
Adam Hendricksen <adam@nird.us>
2013-12-09 18:03:59 -0800

    Renaming files because the tree is already built in the files. This is just the sort

--- spec/sass_sort_spec.rb
+++ spec/sass_sort_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_tree')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_sort')
 require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_creator')
 
 describe "SassTree" do
