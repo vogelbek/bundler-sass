55fc8db2b88b5cf157ff0934a6142827c780328a
Adam Hendricksen <adam@nird.us>
2013-12-10 09:54:58 -0800

    Designing example dependency graphs

--- spec/sass_sort_spec.rb
+++ spec/sass_sort_spec.rb
@@ -7,6 +7,15 @@ describe "SassTree" do
     @test_directory = "test_files"
     @root_directory = Dir.getwd
     Dir.mkdir @test_directory
+    Dir.chdir @test_directory
+    @directories = ["0_nest", 
+                    "0_nest/1_nest_a", 
+                    "0_nest/1_nest_b", 
+                    "0_nest/1_nest_a/2_nest"]
+    
+    @directories.each {|directory| Dir.mkdir(directory)}
+    Dir.chdir @root_directory
+
   end
   before(:each) do
     Dir.chdir @test_directory
@@ -14,16 +23,49 @@ describe "SassTree" do
 
   context "Directed Acyclic Graph" do
     it "should be able to sort a non-nested file structure" do
+      @files = ["_0.scss",
+                "_1.css.sass",
+                "_2.css.scss"]
+      @dependencies = [ [@files[2]],
+                        [@files[0]],
+                        []]
+
       pending "put all partials in the same directory"
     end
 
     it "should be able to sort a nested file structure" do
+      @files = ["_0.scss",
+                "_1.css.sass",
+                "_2.css.scss",
+                "#{@directories[0]}/_3.sass",
+                "#{@directories[0]}/_4.sass",
+                "#{@directories[1]}/_5.sass",
+                "#{@directories[2]}/_6.sass",
+                "#{@directories[3]}/_7.sass"]
+      @dependencies = [ [], #_0.sass
+                        ["#{@directories[0]}/_3.sass"], #_1.sass
+                        ["#{@directories[0]}/_4.sass"], #_2.sass
+                        ["_2.sass", "#{@directories[1]}/_5.sass"], #_3.sass
+                        ["#{@directories[1]}/_5.sass", "#{@directories[2]}/_6.sass"], #_4.sass
+                        [], #_5.sass
+                        ["#{@directories[3]}/_7.sass"], #_6.sass
+                        ["#{@directories[1]}/_5.sass"]] #_7.sass
+      @files.each.with_index do |file, index|
+        build_file_and_hash file, @dependencies[index]
+      end
+      
       pending "build several directories with multiple partials"
     end
   end
 
   context "Cyclic Graph" do
     it "should throw an error when a cyclic graph is detected" do
+      @files = ["_0.scss",
+                "_1.css.sass",
+                "_2.css.scss"]
+      @dependencies = [ [@files[2]],
+                        [@files[0]],
+                        [@files[0]]] #No independent node, graph contains a cycle
       pending "such a graph should be impossible in a Sass project"
     end
   end
