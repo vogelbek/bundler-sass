242c661ab95ef06eb789764837bb1e718881651f
Adam Hendricksen <adam@nird.us>
2013-12-09 18:11:41 -0800

    High level specs for sorting

--- spec/sass_sort_spec.rb
+++ spec/sass_sort_spec.rb
@@ -12,6 +12,22 @@ describe "SassTree" do
     Dir.chdir @test_directory
   end
 
+  context "Directed Acyclic Graph" do
+    it "should be able to sort a non-nested file structure" do
+      pending "put all partials in the same directory"
+    end
+
+    it "should be able to sort a nested file structure" do
+      pending "build several directories with multiple partials"
+    end
+  end
+
+  context "Cyclic Graph" do
+    it "should throw an error when a cyclic graph is detected" do
+      pending "such a graph should be impossible in a Sass project"
+    end
+  end
+
   after(:each) do
     Dir.chdir @root_directory
   end
