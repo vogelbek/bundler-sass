7c921cdc92c39bfecf531079765248a5cd491905
Adam Hendricksen <adam@nird.us>
2013-12-09 18:02:22 -0800

    Should have all the support methods complete

--- lib/sass_reader.rb
+++ lib/sass_reader.rb
@@ -8,8 +8,13 @@ module SassReader
   end
 
   def self.list_partials
-    #list_partials_in_folder(Dir.getwd)
     Dir['**/*.*'].select{|partial| partial =~ /_\S+s[ac]ss\z/}
     #http://rubular.com/r/YCT1k8IaIv, but not at the start of string (for nested files)
+  end
+
+  def self.build_dependency_hash partials
+    partials.inject({}) do |hash, partial|
+      hash.merge self.dependencies(partial)
+    end
   end
 end
