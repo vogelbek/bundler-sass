5db6a941048917fc05d0df5096cb0b8ed05bc64b
Adam Hendricksen <adam@nird.us>
2013-12-10 12:38:02 -0800

    Not quite working right, but not erroring out completely

--- lib/sass_sort.rb
+++ lib/sass_sort.rb
@@ -4,20 +4,33 @@ class SassSort
   include SassReader
   def self.import_order
     @unsorted = SassReader.list_partials
-    @dependencies = SassReader.build_dependency_hash @unsorted
-    depth_first @dependencies
+    depth_first @unsorted, []
   end
 
   private
-  def self.depth_first dependency_array
-    dependency_array.inject([]) do |sorted, file|
-      local_dependencies = SassReader.dependencies file
-      if local_dependencies
-        depth_first local_dependencies
-      else
-        sorted << file
+  def self.depth_first dependency_array, sorted_array
+    if dependency_array.empty? == false
+      puts dependency_array.to_s
+      dependency_array.inject([]) do |sorted, file|
+        local_hash = SassReader.dependencies(file)
+        puts local_hash
+        puts "Sorted: #{sorted}"
+        local_dependencies = local_hash.values.flatten.select do |partial|
+          sorted_array.include?(partial) == false
+        end
+        puts "Depens: #{local_dependencies.to_s}"
+        puts "Empty: #{local_dependencies.empty?}"
+        if local_dependencies.empty?
+          puts "Inserting #{file}"
+          sorted << file
+        else
+          puts "Before: #{sorted}"
+          sorted = sorted + depth_first(local_dependencies.flatten, sorted)
+          puts "After: #{sorted}"
+        end
+        puts "So far: #{sorted}"
+        sorted
       end
-      sorted
     end
   end
 end
--- spec/sass_sort_spec.rb
+++ spec/sass_sort_spec.rb
@@ -32,7 +32,7 @@ describe "SassTree" do
       @files.each.with_index do |file, index|
         build_file_and_hash file, @dependencies[index]
       end
-
+      
       # This particular dependency graph only has a single order it can be loaded in
       SassSort.import_order.should eq [@files[2], @files[1], @files[0]]
     end
