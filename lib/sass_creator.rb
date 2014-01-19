82bb33335d266db73c9cde66423bbff5dfdc9e95
Adam Hendricksen <adam@nird.us>
2014-01-18 18:22:23 -0800

    Making SassFiles file hash readable

--- lib/sass_creator.rb
+++ lib/sass_creator.rb
@@ -1,26 +1,27 @@
 module SassCreator
   class SassFiles
+    attr_reader :file_hash
     def initialize file_hash
       file_hash.each do |file, dependency_list|
         File.open file, 'w'
       end
-      @dependencies = file_hash
+      @file_hash = file_hash
     end
 
     def build_import_comments
-      @dependencies.each do |file, dependency_list|
-        
+      @file_hash.each do |file, dependency_list|
+
         File.open(file, 'r+') do |file|
           dependency_list.each do |dependency|
             file.write "//import \"#{dependency.to_s}\"\n"
           end
         end
-        
+
       end
     end
 
     def build_imports
-      @dependencies.each do |file, dependency_list|
+      @file_hash.each do |file, dependency_list|
 
         File.open(file, 'r+') do |file|
           dependency_list.each do |dependency|
