466f108ef548622fec3104095cdf97916cd2c10d
Adam Hendricksen <adam@nird.us>
2014-01-18 19:43:24 -0800

    Documenting contexts of tools

--- spec/sass_partial_notation_tools_spec.rb
+++ spec/sass_partial_notation_tools_spec.rb
@@ -7,6 +7,15 @@ describe "Sass Partial Notation Tools" do
     @root_directory = Dir.getwd
     Dir.mkdir @test_directory
   end
+
+  context "turn 'partialname' shorthand into '_partialname.sass' partial" do
+    pending "Need to cross-reference partialname with all partials to infer the ending"
+  end
+
+  context "turn '_partialname.sass' partial into 'partialname' shorthand" do
+    pending "Just need to trim _ and .sass"
+  end
+
   after :all do
     teardown_test_directory @test_directory
   end
