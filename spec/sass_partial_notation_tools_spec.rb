011a3c0adc2e6a873509e146e8c2925e8aec907f
Adam Hendricksen <adam@nird.us>
2014-01-18 19:33:58 -0800

    Setting up and tearing down test directory

--- spec/sass_partial_notation_tools_spec.rb
+++ spec/sass_partial_notation_tools_spec.rb
@@ -1,2 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/../lib/sass_partial_notation_tools')
+
+describe "Sass Partial Notation Tools" do
+  before :all do
+    @test_directory = "test_files"
+    @root_directory = Dir.getwd
+    Dir.mkdir @test_directory
+  end
+  after :all do
+    teardown_test_directory @test_directory
+  end
+end
