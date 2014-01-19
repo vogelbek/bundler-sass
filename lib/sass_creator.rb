a62678eefa5cca15e97417faea2d667fa9764eab
Adam Hendricksen <adam@nird.us>
2014-01-18 18:26:43 -0800

    Refactoring mass file writing into private method

--- lib/sass_creator.rb
+++ lib/sass_creator.rb
@@ -9,23 +9,21 @@ module SassCreator
     end
 
     def build_import_comments
-      @file_hash.each do |file, dependency_list|
-
-        File.open(file, 'r+') do |file|
-          dependency_list.each do |dependency|
-            file.write "//import \"#{dependency.to_s}\"\n"
-          end
-        end
-
-      end
+      write_to_all_files "//import "
     end
 
     def build_imports
+      write_to_all_files "@import "
+    end
+
+    private
+
+    def write_to_all_files start_of_line
       @file_hash.each do |file, dependency_list|
 
-        File.open(file, 'r+') do |file|
+        File.open(file, 'r+') do |this_file|
           dependency_list.each do |dependency|
-            file.write "@import \"#{dependency.to_s}\"\n"
+            this_file.write "#{start_of_line}\"#{dependency.to_s}\"\n"
           end
         end
 
