93ff48fb86898d694ca0f5c0f57197e891211757
Adam Hendricksen <adam@nird.us>
2014-01-18 23:12:57 -0800

    Passing test for trimming .scss

--- lib/sass_partial_notation_tools.rb
+++ lib/sass_partial_notation_tools.rb
@@ -1,4 +1,16 @@
 module SassPartial
   def self.shorthandify string
+    clean_underscore! string
+    clean_scss! string
+  end
+
+  private
+
+  def self.clean_underscore! string
+    string.gsub! /\_/, ''
+  end
+
+  def self.clean_scss! string
+    string.gsub! /\.scss/, ''
   end
 end
