155dc0acf614bd76446fd490089bfdac00546b65
Adam Hendricksen <adam@nird.us>
2014-01-22 20:41:57 -0800

    Inserting an underscore to the right place in a string

--- lib/sass_partial_notation_tools.rb
+++ lib/sass_partial_notation_tools.rb
@@ -4,11 +4,20 @@ module SassPartial
     clean! string, cleaning_agents
   end
 
+  def self.partialize string
+    string.insert(find_underscore(string), '_')
+  end
+
   private
 
   def self.clean! string, cleaning_agents
     cleaning_agents.inject( string ) do |cleaner_string, cleaning_regex|
       cleaner_string.gsub cleaning_regex, ''
     end
+  end
+
+  def self.find_underscore string
+    string.index /\w*$/
+    # http://rubular.com/r/5hoxyVTsso
   end
 end
